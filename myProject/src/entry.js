1f62e98e304a23437a655106afb0f4e4a2469f8f
JestemBartek <maciek.bartynski@gmail.com>
2018-08-16 21:13:04 +0200

    navbar

--- myProject/src/entry.js
+++ myProject/src/entry.js
@@ -1,5 +1,23 @@
 import './vars.scss';
 import './mixes.scss';
 import './generals.scss';
-import './nav/nav.scss';
-import './nav/nav.js';
+import './a-header.scss';
+document.addEventListener('DOMContentLoaded', function () {
+    let hamburger = document.querySelector('.grid-item-hamburger');
+    let mobilemenu = document.querySelector('.head_navbar_nav_left-belt_menu-list');
+    let iterator=0;
+    hamburger.addEventListener('click', function(){
+        toggleMenu();
+    });
+    function toggleMenu(){
+        if (!iterator) mobilemenu.classList.add('toggle-on-mobile-devices');
+        else mobilemenu.classList.remove('toggle-on-mobile-devices');
+        iterator = iterator? 0:1;
+    }
+
+    let fixedmenu = document.querySelector('.head_navbar_nav');
+
+    window.addEventListener('scroll', function () {
+        fixedmenu.classList.add('onpagemove');
+    });
+})

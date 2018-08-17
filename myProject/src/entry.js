c9c76033dfc9aa40fae07a99bfd6dda71c1a6313
JestemBartek <maciek.bartynski@gmail.com>
2018-08-17 07:30:44 +0200

    rano

--- myProject/src/entry.js
+++ myProject/src/entry.js
@@ -1,34 +1,8 @@
-import './vars.scss';
-import './mixes.scss';
-import './generals.scss';
-import './a-header.scss';
-import './b-header.scss';
-document.addEventListener('DOMContentLoaded', function () {
-    let hamburger = document.querySelector('.grid-item-hamburger');
-    let mobilemenu = document.querySelector('.head_navbar_nav_left-belt_menu-list');
-    let iterator=0;
-    hamburger.addEventListener('click', function(){
-        toggleMenu();
-    });
-    function toggleMenu(){
-        if (!iterator) mobilemenu.classList.add('toggle-on-mobile-devices');
-        else mobilemenu.classList.remove('toggle-on-mobile-devices');
-        iterator = iterator? 0:1;
-    }
+import "./vars.scss";
+import "./mixes.scss";
+import "./generals.scss";
+import "./a-header.scss";
+import "./b-header.scss";
 
-    let fixedmenu = document.querySelector('.head_navbar_nav');
-    let fixedmenuContainer = document.querySelector('.head_navbar');
-    window.addEventListener('scroll', function () {
-        let barHeight = scrollPositionDetection();
-        if (window.pageYOffset>barHeight) {
-             fixedmenu.classList.add('onpagemove');
-             fixedmenuContainer.style.height = fixedmenu.offsetHeight + "px";
-        }else {
-             fixedmenu.classList.remove('onpagemove');
-             fixedmenuContainer.style.height = "";
-        }
-    });
-    function scrollPositionDetection () {
-       return fixedmenu.offsetHeight;
-    }
-})
+import "./a-navbar.js";
+import "./animation.js";
